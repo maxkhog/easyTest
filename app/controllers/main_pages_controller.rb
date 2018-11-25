9f73833a7e96d302c3060a0558ae65ed2f16e677
maxkhog <maxkhog@gmail.com>
2018-11-25 17:27:31 +0800

    -add main pages

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,4 @@
+class MainPagesController < ApplicationController
+  def main
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(title = '')
+        base_title = 'Easy App'
+        if title.empty?
+            base_title 
+        else
+            title + " | " + base_title
+        end
+    end
 end
